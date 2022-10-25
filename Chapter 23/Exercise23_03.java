78ae012631bf6f9ad3252f0c15ca62037234227e
jmcnu <105464822+jmcnu@users.noreply.github.com>
2022-10-25 15:27:33 -0600

    Add files via upload

---
+++ Chapter 23/Exercise23_03.java
@@ -0,0 +1,26 @@
+/*
+Author: 
+Date: 
+
+Description: 
+*/
+import java.util.Comparator;
+
+public class Exercise23_03 {
+  public static void main(String[] args) {
+    Integer[] list = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
+    quickSort(list);
+    for (int i = 0; i < list.length; i++) {
+      System.out.print(list[i] + " ");
+    }
+
+    System.out.println();
+    Circle[] list1 = {new Circle(2), new Circle(3), new Circle(2),
+                     new Circle(5), new Circle(6), new Circle(1), new Circle(2),
+                     new Circle(3), new Circle(14), new Circle(12)};
+    quickSort(list1, new GeometricObjectComparator());
+    for (int i = 0; i < list1.length; i++) {
+      System.out.println(list1[i] + " ");
+    }
+  }
+}
